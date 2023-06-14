190ea7882aaacd16d11dff11fcf62e1256a099c2
d.karyakin <3754479555552@gmail.com>
2023-06-14 12:51:44 +0300

    feat: creat consumer

---
+++ Consumer/Receiver.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Consumer
+{
+    public class Receiver
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" }; // create the factory
+            using (var connection = factory.CreateConnection()) // open the connection
+            using (var chanel = connection.CreateModel()) // open the chanel
+            {
+                chanel.QueueDeclare( // declare the queue
+                    queue: "BasicTest",
+                    durable: false,
+                    exclusive: false,
+                    autoDelete: false,
+                    arguments: null
+                );
+                var consumer = new EventingBasicConsumer(chanel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body.ToArray();
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine($"Recieved message: {message}");
+                };
+
+                chanel.BasicConsume(
+                    queue: "BasicTest",
+                    autoAck: true,
+                    consumer: consumer
+                );
+                
+                Console.WriteLine("Press key to exit consumer");
+                Console.ReadKey();
+            }
+        }
+    }
+}
