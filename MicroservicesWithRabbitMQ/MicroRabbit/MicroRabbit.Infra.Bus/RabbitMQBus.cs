4d3cf3ffd069334fdec4147ad5f0157837a4e4b4
d.karyakin <3754479555552@gmail.com>
2023-06-14 15:54:13 +0300

    feat: created publish and Subscribe

---
+++ MicroservicesWithRabbitMQ/MicroRabbit/MicroRabbit.Infra.Bus/RabbitMQBus.cs
@@ -0,0 +1,81 @@
+﻿using System.Text;
+using MediatR;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Commands;
+using MicroRabbit.Domain.Core.Events;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+
+namespace MicroRabbit.Infra.Bus;
+
+public sealed class RabbitMQBus : IEventBus
+{
+    private readonly IMediator _mediator;
+    private readonly Dictionary<string, List<Type>> _handlers;
+    private readonly List<Type> _eventTypes;
+
+    public RabbitMQBus(IMediator mediator)
+    {
+        _mediator = mediator;
+        _handlers = new Dictionary<string, List<Type>>();
+        _eventTypes = new List<Type>();
+    }
+
+    public Task SendCommand<T>(T command) where T : Command
+    {
+        return _mediator.Send(command);
+    }
+
+    public void Publish<T>(T @event) where T : Event
+    {
+        var factory = new ConnectionFactory() { HostName = "localhost" };
+        using (var connection = factory.CreateConnection())
+        using (var chanel = connection.CreateModel())
+        {
+            var eventName = @event.GetType().Name;
+            chanel.QueueDeclare(
+                queue: eventName,
+                durable: false,
+                exclusive: false,
+                arguments: null
+            );
+
+            var message = JsonConvert.SerializeObject(@event);
+            var body = Encoding.UTF8.GetBytes(message);
+
+            chanel.BasicPublish(
+                exchange: string.Empty,
+                routingKey: eventName,
+                basicProperties: null,
+                body: body
+            );
+        }
+    }
+
+
+    public void Subscribe<T, TH>() where T : Event where TH : IEventHandler<T>
+    {
+        var eventName = typeof(T).Name;
+        var handlerType = typeof(TH);
+
+        if (!_eventTypes.Contains(typeof(T)))
+        {
+            _eventTypes.Add(typeof(T));
+        }
+
+        if (!_handlers.ContainsKey(eventName))
+        {
+            _handlers.Add(eventName, new List<Type>());
+        }
+
+        if (_handlers[eventName].Any(s => s.GetType() == handlerType))
+        {
+            throw new ArgumentException($"Handler type {handlerType.Name} already is registered for '{eventName}'",
+                nameof(handlerType));
+        }
+        
+        _handlers[eventName].Add(handlerType);
+
+       // StartBasicConsume<T>();
+    }
+}
